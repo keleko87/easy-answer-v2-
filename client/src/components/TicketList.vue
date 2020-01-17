a7d1da88256224d54a0c07da086ca5a262793e5e
mmartinsa <mmartinsa@minsait.com>
2020-01-17 14:58:26 +0100

    feat(): create card ticket WIP

--- client/src/components/TicketList.vue
+++ client/src/components/TicketList.vue
@@ -5,19 +5,19 @@
       <div v-if="tickets.length === 0" class="ticket-preview">
         No tickets are here... yet.
       </div>
-      <div
-        v-for="(ticket) in tickets"
-        :ticket="ticket"
-        :key="ticket._id">
-      
-        <div class="card" style="width: 18rem;">
-          <template v-if="ticket.image !== 'nofile'">
-            <img class="card-img-top" :src="ticket.image" alt="Card image cap">
-          </template>
+    </div>
+
+    <!-- <div class="container">
+      <div class="row" v-for="(ticket) in tickets" :ticket="ticket" :key="ticket._id">
+        <div class="col-md-2"></div>
+        <div class="card col-md-8">
           <div class="card-body">
             <h5 class="card-title">{{ ticket.title }}</h5>
             <p class="card-text">{{ ticket.description }}</p>
           </div>
+          <template v-if="ticket.image !== 'nofile'">
+            <img class="card-img-top" :src="ticket.image" alt="Card image cap">
+          </template>
           <ul class="list-group list-group-flush">
             <li class="list-group-item">Cras justo odio</li>
             <li class="list-group-item">Dapibus ac facilisis in</li>
@@ -30,7 +30,45 @@
         </div>
       
       </div>
+    </div> -->
+
+    <div class="container-fluid">
+
+      <template v-if="tickets.length">
+        <div class="row" v-for="ticket in tickets" :ticket="ticket" :key="ticket._id">         
+ 
+          <div class="col-xs-12 col-md-8 col-md-offset-2">
+            <div class="panel panel-white post panel-shadow">
+                <div class="post-heading">
+                    <div class="pull-left image">
+                        <img :src="ticket.creatorId[0].imgAvatar" 
+                             class="img-circle avatar" alt="user profile image">
+                    </div>
+                    <div class="pull-left meta">
+                        <div class="title h5">
+                            <a href="#"><b>{{ ticket.creatorId[0].username }}</b></a>
+                            posted a question
+                        </div>
+                        <h6 class="text-muted time">Created:{{ ticket.created_at }}</h6>
+                    </div>
+                </div>
+                <div class="post-description">
+
+                  <h2>
+                    <router-link to="`/ticket/${ticket._id}`">{{ ticket.title }}</router-link>
+                  </h2>
+
+                    <p class="limit-words">{{ ticket.content }}</p>
+
+                    <div class="stats">
+                        <span class="label lb-lg">{{ ticket.tags }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+  </template>
+</div>
   </div>
 </template>
 
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div class="jumbotron">
       <h1>Easy Answer 2</h1>
-      <p class="shadow">Join the web development community of Easy Answer to share your knowledge, learn or discuss!</p>
+      <h4>Join the web development community of Easy Answer to share your knowledge, learn or discuss!</h4>
     </div>
     <div>
       <ticket-list :type="''"></ticket-list>
@@ -28,33 +28,10 @@ export default {
 
 <style scoped>
   .jumbotron {
-    /*border-radius:10px!important;*/
-    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.4);
     background-size: cover;
     background-position: center center;
     background-repeat: no-repeat;
-    background-image: url('/assets/img/blurry.jpg');
-    color: #FFF;
-    position: relative;
-    height: 100%;
-    padding: 20px;
-  }
-
-  .jumbotron h1 {
-    color: #ffeb3b;
-    text-shadow: -1px 0 black, 0 1px black;
-    text-align: center;
-  }
-
-  .jumbotron p {
-    text-align: center;
-  }
-
-  .jumbotron .btn {
-    border-radius: 0px!important;
-    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.4);
-    padding: 10px 40px;
-    text-align: center;
+    /* background-image: url('/public/img/android-chorme-192x192.png'); */
   }
 
 </style>
