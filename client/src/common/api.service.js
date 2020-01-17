88bdc3a962215667564a005d663b8fb0edce406c
mmartinsa <mmartinsa@minsait.com>
2020-01-17 14:13:22 +0100

    feat(): Config environments in project

--- client/src/common/api.service.js
+++ client/src/common/api.service.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import JwtService from '../common/jwt.service';
-import { API_URL } from '../common/config';
+
+const API_URL = process.env.VUE_APP_API;
 
 const ApiService = {
   init() {
+    console.log(process.env);
     Vue.use(VueAxios, axios);
     Vue.axios.defaults.baseURL = API_URL;
   },
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/img/mevn.jpg" /> -->
-    <ticket-list :type="''"></ticket-list>
+    <div class="jumbotron">
+      <h1>Easy Answer 2</h1>
+      <p class="shadow">Join the web development community of Easy Answer to share your knowledge, learn or discuss!</p>
+    </div>
+    <div>
+      <ticket-list :type="''"></ticket-list>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import TicketList from '../components/TicketList';
 
 export default {
   name: 'home',
+  data() {
+    return {
+      env: process.env
+    };
+  },
   components: {
     TicketList,
   }
 };
 </script>
+
+<style scoped>
+  .jumbotron {
+    /*border-radius:10px!important;*/
+    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.4);
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-image: url('/assets/img/blurry.jpg');
+    color: #FFF;
+    position: relative;
+    height: 100%;
+    padding: 20px;
+  }
+
+  .jumbotron h1 {
+    color: #ffeb3b;
+    text-shadow: -1px 0 black, 0 1px black;
+    text-align: center;
+  }
+
+  .jumbotron p {
+    text-align: center;
+  }
+
+  .jumbotron .btn {
+    border-radius: 0px!important;
+    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.4);
+    padding: 10px 40px;
+    text-align: center;
+  }
+
+</style>
